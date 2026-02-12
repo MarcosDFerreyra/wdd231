94c1d1704b961ab749c45fa07b1dfad60771d752
MarcosDFerreyra <marcosferrey30>
2026-02-12 19:59:00 -0300

    cosas

---
+++ project/scripts/base.js
@@ -0,0 +1,12 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = currentYear;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const navbutton = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('#nav-bar');
+
+navbutton.addEventListener('click', () => {
+    navlinks.classList.toggle('show');
+    navbutton.classList.toggle('show');
+});
+
